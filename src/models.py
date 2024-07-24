c09ac47fc7006e4071e9bcc488b99d69e3f709bb
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-07-24 15:40:55 +0000

    Project completed

--- src/models.py
+++ src/models.py
@@ -8,22 +8,25 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
     first_name = Column(String(250), nullable=False)
     last_name = Column(String(250), nullable=False)
     created_at = Column(DateTime, nullable=False)
-    favorite = relationship('Favorite', backref='user', lazy=True)
+    favorites = relationship('Favorite', backref='users', lazy=True)
 
 class Favorite(Base):
-    __tablename__ = 'favorite'
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    people_id = Column(Integer, ForeignKey('people.id'), nullable=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'), nullable=True)
     
 class Planet(Base):
-    __tablename__ = 'planet'
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), unique=True, nullable=False)
     gravity = Column(Integer, nullable=False)
@@ -31,25 +34,22 @@ class Planet(Base):
     terrain = Column(String(250), nullable=False)
     climate = Column(String(250), nullable=False)
     diameter = Column(Integer, nullable=False)
-    favorite_id = Column(Integer, ForeignKey('favorite.id'), nullable=True)
     
-class Character(Base):
-    __tablename__ = 'character'
+class People(Base):
+    __tablename__ = 'people'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     gender = Column(String(250), nullable=False)
     height = Column(Float, nullable=False)
     weight = Column(Float, nullable=False)
-    favorite_id = Column(Integer, ForeignKey('favorite.id'), nullable=True)
     
 class Vehicle(Base):
-    __tablename__ = 'vehicle'
+    __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
     manufacturer = Column(String(250), nullable=False)
     model = Column(String(250), nullable=False)
     capacity = Column(Integer, nullable=False)
     cost = Column(Float, nullable=False)
-    favorite_id = Column(Integer, ForeignKey('favorite.id'), nullable=True)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
